68991ad4e9aae537ef4ce63246aa16125947be31
martinwachira <martinnwachira@gmail.com>
2024-02-28 13:42:00 +0300

    added page header

---
+++ src/Components/PageHeader.js
@@ -0,0 +1,16 @@
+import "../App.css";
+
+import React from "react";
+import logo from "../saf-logo.png";
+
+const PageHeader = () => {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+    </>
+  );
+};
+
+export default PageHeader;
